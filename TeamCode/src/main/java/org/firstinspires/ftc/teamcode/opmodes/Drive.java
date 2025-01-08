8d8ac053f74519c40786eb13019a9c7e2935a633
siraj <srjbadri2008@gmail.com>
2025-01-08 15:25:02 +0200

    LES go

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Drive.java
@@ -7,18 +7,31 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Drive", group = "TeleOp")
 public class Drive extends LinearOpMode {
+
+    private static final int STAGE_1 = 900;
+    private static final int STAGE_2 = 1800;
+    private static final int STAGE_3 = 2700;
+
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor slide1 = hardwareMap.dcMotor.get("slide1");
         DcMotor slide2 = hardwareMap.dcMotor.get("slide2");
+
         DcMotor arm = hardwareMap.dcMotor.get("arm");
+
         DcMotor frontLeftMotor = hardwareMap.dcMotor.get("fl");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("bl");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("fr");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("br");
 
+        slide2.setDirection(DcMotor.Direction.REVERSE);
+        slide1.setDirection(DcMotor.Direction.REVERSE);
+
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        slide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
@@ -46,6 +59,14 @@ public class Drive extends LinearOpMode {
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
 
+            if (gamepad1.a) {
+                moveToPosition(slide1, slide2, STAGE_1); // Move to stage 1
+            } else if (gamepad1.b) {
+                moveToPosition(slide1, slide2, STAGE_2); // Move to stage 2
+            } else if (gamepad1.y) {
+                moveToPosition(slide1, slide2, STAGE_3); // Move to stage 3
+            }
+        }
             if(gamepad1.right_bumper){
                 slide1.setPower(0.7);
                 slide2.setPower(1);
@@ -68,5 +89,25 @@ public class Drive extends LinearOpMode {
                 arm.setPower(0);
             }
         }
+    private void moveToPosition(DcMotor slide1, DcMotor slide2, int targetPosition) {
+        int currentPosition = slide2.getCurrentPosition();
+        int direction = (targetPosition > currentPosition) ? 1 : -1; // Determine the direction
+
+        slide2.setTargetPosition(targetPosition);
+        slide2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slide2.setPower(1); // Adjust power based on direction
+        slide1.setPower(direction * 0.7);
+
+        while (opModeIsActive() && slide2.isBusy()) {
+            telemetry.addData("Target", targetPosition);
+            telemetry.addData("Current Position", slide2.getCurrentPosition());
+            telemetry.update();
+        }
+
+        // Stop the motors after reaching the target
+        slide1.setPower(0);
+        slide2.setPower(0);
+        telemetry.addData("Reached", "Pos " + slide2.getCurrentPosition());
+        telemetry.update();
     }
-}
+    }
