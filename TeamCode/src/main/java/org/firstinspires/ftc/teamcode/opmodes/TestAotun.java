9117a504d7c7f254b9aae222a0c180063a513cf2
siraj <srjbadri2008@gmail.com>
2025-02-07 19:50:42 +0200

    Updatesss

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAotun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAotun.java
@@ -35,7 +35,7 @@ public class TestAotun extends LinearOpMode {
         PinpointDrive drive = new PinpointDrive(hardwareMap, initialPose);
 
         TrajectoryActionBuilder sp = drive.actionBuilder(initialPose)
-                .splineToConstantHeading(new Vector2d(3.45, -33.81), Math.toRadians(90.00));
+                .splineToConstantHeading(new Vector2d(3, -28), Math.toRadians(90.00));
 
 
         TrajectoryActionBuilder sp2 = drive.actionBuilder(new Pose2d(3.45, -33.81, Math.toRadians(90.00)))
