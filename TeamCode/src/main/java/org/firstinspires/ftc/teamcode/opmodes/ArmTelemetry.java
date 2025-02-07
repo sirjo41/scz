0fbf16fb6d92ebc9a21b8b0caec97239f85d37b5
siraj <srjbadri2008@gmail.com>
2025-02-07 18:46:44 +0200

    Updatesss

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTelemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTelemetry.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "ArmTelemetry", group = "TeleOp")
 public class ArmTelemetry extends LinearOpMode {
@@ -15,7 +16,9 @@ public class ArmTelemetry extends LinearOpMode {
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        DcMotor slide1 = hardwareMap.dcMotor.get("slide1");
+        DcMotor slide2 = hardwareMap.dcMotor.get("slide2");
+        Servo wrist = hardwareMap.servo.get("Wrist");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         
@@ -25,6 +28,10 @@ public class ArmTelemetry extends LinearOpMode {
 
         while (opModeIsActive()) {
             telemetry.addData("Arm Position", arm.getCurrentPosition());
+            telemetry.addData("slide1" , slide1.getCurrentPosition());
+            telemetry.addData("slide2",slide2.getCurrentPosition());
+            telemetry.addData("wrist",wrist.getPosition());
+
             telemetry.update();
         }
     }
