1cd57a31786f69f141be92350a6c8efa5a7323e0
siraj <srjbadri2008@gmail.com>
2025-02-07 17:51:29 +0200

    Updatesss

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -65,12 +65,12 @@ public class MecanumDrive {
         // drive model parameters
         public double inPerTick = 1.0; // If you're using OTOS/Pinpoint leave this at 1 (all values will be in inches, 1 tick = 1 inch)
         public double lateralInPerTick = 0.6163634640605182; // Tune this with LateralRampLogger (even if you use OTOS/Pinpoint)
-        public double trackWidthTicks = 0;
+        public double trackWidthTicks = 15;
 
         // feedforward parameters (in tick units)
         public double kS = 1.15308354194165;
         public double kV = 0.16963365573038867;
-        public double kA = 0.0;
+        public double kA = 0.015545757;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAotun.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Trajectory;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.acmerobotics.roadrunner.ftc.GoBildaPinpointDriverRR;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.PinpointDrive;
+import org.firstinspires.ftc.teamcode.actions.Arm;
+import org.firstinspires.ftc.teamcode.actions.IntakeServos;
+import  org.firstinspires.ftc.teamcode.actions.Slides;
+
+@Disabled
+@Autonomous(name = "AutonT",group = "Test")
+public class TestAotun extends LinearOpMode {
+
+    GoBildaPinpointDriverRR odo;
+    @Override
+    public void runOpMode() {
+        odo = hardwareMap.get(GoBildaPinpointDriverRR.class, "pinpoint");
+        Slides slides = new Slides(hardwareMap);
+        Arm arm = new Arm(hardwareMap);
+        IntakeServos intakeServos = new IntakeServos(hardwareMap);
+
+        Pose2d initialPose = new Pose2d(24.31, -68.09, Math.toRadians(90.00));
+        PinpointDrive drive = new PinpointDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder sp = drive.actionBuilder(initialPose)
+                .splineToConstantHeading(new Vector2d(1.0, -40.0), Math.toRadians(90.00));
+
+        telemetry.addData("Status","DONE");
+        telemetry.update();
+        waitForStart();
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new ParallelAction(
+                        sp.build()
+                )
+        );
+
+
+        telemetry.addData("Status", "Completed");
+        telemetry.update();
+    }
+}
