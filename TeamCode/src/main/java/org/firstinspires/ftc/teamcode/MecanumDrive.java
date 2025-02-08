f03bfea8f5e8149a872c8db25df4d5c923544e73
siraj <srjbadri2008@gmail.com>
2025-02-08 13:50:40 +0200

    Autoooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -78,8 +78,8 @@ public class MecanumDrive {
         public double maxProfileAccel = 100;
 
         // turn profile parameters (in radians)
-        public double maxAngVel = Math.PI; // shared with path
-        public double maxAngAccel = Math.PI;
+        public double maxAngVel = 1.5 * Math.PI; // shared with path
+        public double maxAngAccel = 1.5 * Math.PI;
 
         // path controller gains
         public double axialGain = 0.0;
