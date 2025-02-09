85cdf9846b1370c39ed0c7c170fc067a1bebadff
siraj <srjbadri2008@gmail.com>
2025-02-09 23:58:05 +0200

    Autoooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAotun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAotun.java
@@ -1,59 +1,130 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.acmerobotics.roadrunner.ftc.GoBildaPinpointDriverRR;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-
-import org.firstinspires.ftc.teamcode.MecanumDrive;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.teamcode.PinpointDrive;
-import org.firstinspires.ftc.teamcode.actions.Arm;
-import org.firstinspires.ftc.teamcode.actions.IntakeServos;
-import  org.firstinspires.ftc.teamcode.actions.Slides;
 
+import kotlin.math.MathKt;
 
 @Autonomous(name = "TEST",group = "Test")
 public class TestAotun extends LinearOpMode {
+    private static final int S_DF = 0;
+    private static final int S_OUTTAKE = 800;
+
+    private static final int A_DF = 0;
+    private static final int A_1 = 800;
+    private static final int A_2 = 1600;
+    private static final int A_3 = 2200;
+
+    private static final double WR_DF = 0.5;
+    private static final double WR_CLOSED = 0.0;
+    private static final double WR_OPEN = 1.0;
 
     GoBildaPinpointDriverRR odo;
     @Override
     public void runOpMode() {
         odo = hardwareMap.get(GoBildaPinpointDriverRR.class, "pinpoint");
-        IntakeServos intakeServos = new IntakeServos(hardwareMap);
-        Slides slides = new Slides(hardwareMap);
-        Arm arm = new Arm(hardwareMap);
-        Pose2d initialPose = new Pose2d(26.91, -62.25, Math.toRadians(90.00));
+        Pose2d initialPose = new Pose2d(14.25, -62.13, Math.toRadians(90.00));
         PinpointDrive drive = new PinpointDrive(hardwareMap, initialPose);
 
         TrajectoryActionBuilder sp = drive.actionBuilder(initialPose)
-                .splineTo(new Vector2d(4.57, -34.02), Math.toRadians(90.00));
+                .strafeToConstantHeading(new Vector2d(12.57, -43));
 
-        intakeServos.resetWrist();
-        telemetry.addData("Status","DONE");
+
+        TrajectoryActionBuilder sp2 = drive.actionBuilder(new Pose2d(12.57, -43, Math.toRadians(90.00)))
+                .strafeTo(new Vector2d(32,-43))
+                .strafeTo(new Vector2d(32,-15))
+                .strafeTo(new Vector2d(45,-15))
+                .strafeTo(new Vector2d(45,-50))
+                .strafeTo(new Vector2d(45,-15))
+                .strafeTo(new Vector2d(55,-15))
+                .strafeTo(new Vector2d(55,-50))
+                .strafeTo(new Vector2d(55,-15))
+                .strafeTo(new Vector2d(61,-15))
+                .strafeTo(new Vector2d(61,-50));
+
+
+        TrajectoryActionBuilder sp3 = drive.actionBuilder(new Pose2d(61,-50, Math.toRadians(90.00)))
+                .strafeToLinearHeading(new Vector2d(30,-55),Math.toRadians(-80.00));
+
+        TrajectoryActionBuilder sp4 = drive.actionBuilder(new Pose2d(30,-55, Math.toRadians(-80.00)))
+                .strafeToLinearHeading(new Vector2d(12, -43), Math.toRadians(90.00));
+
+        TrajectoryActionBuilder sp5 = drive.actionBuilder(new Pose2d(12,-43, Math.toRadians(90.00)))
+                .strafeToLinearHeading(new Vector2d(30,-55),Math.toRadians(-80.00));
+        DcMotor arm = hardwareMap.dcMotor.get("arm");
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        DcMotor slide1 = hardwareMap.dcMotor.get("slide1");
+        DcMotor slide2 = hardwareMap.dcMotor.get("slide2");
+        Servo wrist = hardwareMap.servo.get("Wrist");
+
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+        slide2.setDirection(DcMotor.Direction.REVERSE);
+        slide1.setDirection(DcMotor.Direction.FORWARD);
+
+
+        telemetry.addData("Status","INT");
         telemetry.update();
+
         waitForStart();
-        if (isStopRequested()) return;
 
         Actions.runBlocking(
-                new ParallelAction(
-                        arm.goToStage1(),
-                        slides.goToStage2(),
-                        sp.build()
+                new SequentialAction(
+                        sp.build(),
+                        sp2.build(),
+                        sp3.build(),
+                        sp4.build(),
+                        sp5.build(),
+                        sp4.build(),
+                        sp5.build(),
+                        sp4.build(),
+                        sp5.build(),
+                        sp4.build()
                 )
         );
 
-        arm.holdPosition();
+
+
+        if (isStopRequested()) return;
 
         telemetry.addData("Status", "Completed");
         telemetry.update();
+    }
+
+    private void moveArmTo(DcMotor arm,int pos) {
+        arm.setTargetPosition(pos);
+
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(1);
+
+    }
+
+    private  void  moveSlideToPos(DcMotor slides1, DcMotor slides2,int pos){
+        slides1.setTargetPosition(pos);
+        slides2.setTargetPosition(pos);
+        slides1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slides2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        slides1.setPower(0.7);
+        slides2.setPower(1);
+
+        do {
+            telemetry.addData("Target", pos);
+            telemetry.addData("Current Position Slide1", slides1.getCurrentPosition());
+            telemetry.addData("Current Position Slide2", slides2.getCurrentPosition());
+            telemetry.update();
+        } while (opModeIsActive() && slides1.isBusy() && slides2.isBusy());
+        slides1.setPower(0);
+        slides2.setPower(0);
     }
 }
