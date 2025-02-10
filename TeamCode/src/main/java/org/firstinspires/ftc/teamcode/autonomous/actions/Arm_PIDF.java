d40aadb23b10fa3d46f631841bc6f7077c2abe1a
Siraj Elbadri <srjbadri2008@gmail.com>
2025-02-10 23:07:37 +0200

    Update Arm_PIDF.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/actions/Arm_PIDF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/actions/Arm_PIDF.java
@@ -1,15 +1,10 @@
 package org.firstinspires.ftc.teamcode.autonomous.actions;
 
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
-import com.arcrobotics.ftclib.controller.PIDFController;
-import com.arcrobotics.ftclib.hardware.motors.MotorEx;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @Config
@@ -19,35 +14,35 @@ public class Arm_PIDF extends OpMode {
 
     public static double p = 0, i = 0, d = 0;
     public static double f = 0;
-
     public static double target = 0;
 
     private final double ticks_in_deg = 2688.5 / 360.0;
-
     private DcMotorEx arm;
+
     @Override
     public void init() {
         armController = new PIDController(p, i, d);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-
-        arm = hardwareMap.get(DcMotorEx.class,"arm");
-
+        arm = hardwareMap.get(DcMotorEx.class, "arm");
     }
 
     @Override
     public void loop() {
-        armController.setPID(p,i,d);
+        armController.setPID(p, i, d);
         int arm_pos = arm.getCurrentPosition();
-        double pid = armController.calculate(arm_pos,target);
-        double ff = Math.cos(Math.toRadians(target/ ticks_in_deg)) * f;
-
+        double pid = armController.calculate(arm_pos, target);
+        double ff = Math.cos(Math.toRadians(target / ticks_in_deg)) * f;
         double power = pid + ff;
 
         arm.setPower(power);
 
-        telemetry.addData("pos",arm_pos);
-        telemetry.addData("target",target);
-        telemetry.addData("power",power);
+        telemetry.addData("pos", arm_pos);
+        telemetry.addData("target", target);
+        telemetry.addData("power", power);
+        telemetry.addData("p", p);
+        telemetry.addData("i", i);
+        telemetry.addData("d", d);
+        telemetry.addData("f", f);
         telemetry.update();
     }
 }
