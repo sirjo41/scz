24234b828f1c6d56514be3e0312fc67ed5ff9c45
siraj <srjbadri2008@gmail.com>
2025-02-11 14:49:51 +0200

    Auton Updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/actions/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/actions/Arm.java
@@ -26,9 +26,9 @@ public class Arm {
 
     // Arm Position Stages (Using Ticks)
     public  static final int STAGE_0 = 0;
-    public static final int STAGE_1 = -200;  // 0 degrees
-    public static final int STAGE_2 = 300;   // 90 degrees
-    public static final int STAGE_3 = 500;   // 160 degrees
+    public static final int STAGE_1 = -50;  // 0 degrees
+    public static final int STAGE_2 = 50;   // 90 degrees
+    public static final int STAGE_3 = 100;   // 160 degrees
 
     private double targetPosition = STAGE_0; // Default target
 
