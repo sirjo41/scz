1401d250df11e0a8a4376ad38ef0f81e8baa9969
Siraj Elbadri <srjbadri2008@gmail.com>
2025-02-15 01:29:25 +0200

    Update AutoR.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoR.java
@@ -52,16 +52,16 @@ public class AutoR extends LinearOpMode {
 
         TrajectoryActionBuilder InTakePos1 = drive.actionBuilder( new Pose2d(OutTake.x,OutTake.y,Math.toRadians(90.00)))
                 .strafeTo(new Vector2d(InTake.x, InTake.y))
-                .turn(3);
+                .turn(Math.PI / 2);
 
 
         TrajectoryActionBuilder OutTakePos2 = drive.actionBuilder(new Pose2d(InTake.x, InTake.y, Math.toRadians(270.0)))
                 .strafeTo(new Vector2d(OutTake.x, OutTake.y))
-                .turn(3);
+                .turn(Math.PI / 2);
 
         TrajectoryActionBuilder InTakePos2 = drive.actionBuilder(new Pose2d(OutTake.x, OutTake.y, Math.toRadians(90.0)))
                 .strafeTo(new Vector2d(InTake.x,InTake.y))
-                .turn(3);
+                .turn(Math.PI / 2);
 
 
         telemetry.addData("Status","READDDYYYY ");
