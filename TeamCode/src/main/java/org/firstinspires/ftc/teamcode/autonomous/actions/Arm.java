85748387c4b7c99c1c1f14a9d1732d22383dbfe9
siraj <srjbadri2008@gmail.com>
2025-02-14 23:10:15 +0200

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/actions/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/actions/Arm.java
@@ -23,8 +23,8 @@ public class Arm {
     // Arm Position Stages (Using Ticks)
     public static double STAGE_0 = 0;
     public static double STAGE_INTAKE = -50;  // 0 degrees
-    public static double STAGE_OUTTAKE = 70;// 90 degrees// 180 degrees;
-    public static double STAGE_OUTTAKE2 = 74;
+    public static double STAGE_OUTTAKE = 76;// 90 degrees// 180 degrees;
+    public static double STAGE_OUTTAKE2 = 77;
     public static double STAGE_INTAKE2 = 77;
 
     private volatile  double targetPosition = STAGE_0; // Default target
