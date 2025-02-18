f931a3cc707e26fa010092254d9161f78f703671
siraj <srjbadri2008@gmail.com>
2025-02-18 20:35:03 +0200

    drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/pos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/pos.java
@@ -1,12 +1,19 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 @TeleOp(name = "fingers")
 public class pos extends LinearOpMode {
+
+    public volatile double figners_pos = 0;
+    public volatile double elbow_pos = 0;
+    public volatile double shoulder_pos = 0;
+
     @Override
     public void runOpMode() throws InterruptedException {
         Servo fingers = hardwareMap.servo.get("fingers");
@@ -21,9 +28,9 @@ public class pos extends LinearOpMode {
 
         while (opModeIsActive()) {
 
-            fingers.setPosition(0);
-            elbow.setPosition(0);
-            shoulder.setPosition(0);
+            fingers.setPosition(figners_pos);
+            elbow.setPosition(elbow_pos);
+            shoulder.setPosition(shoulder_pos);
 
             telemetry.addData("Fingers pos", fingers.getPosition());
             telemetry.addData("elbow pos", elbow.getPosition());
