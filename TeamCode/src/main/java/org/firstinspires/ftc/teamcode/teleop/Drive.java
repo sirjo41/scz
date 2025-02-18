c29c4d1ac1d29aa3ee30a4a75606d7551db55321
siraj <srjbadri2008@gmail.com>
2025-02-18 18:05:55 +0200

    drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Drive.java
@@ -14,13 +14,13 @@ public class Drive extends LinearOpMode {
     public volatile  static double f = -0.05;
     private final double ticks_in_deg = 2688.5 / 360.0;
 
-    private  static  final  int S_INTAKE = 800;
+    //private  static  final  int S_INTAKE = 800;
 
-    private static final double WR_DF = 0.7;
-    private static final double WR_CLOSED = 0.2;
-    private static final double WR_OPEN = 0.4;
+    //private static final double WR_DF = 0;
+    //private static final double WR_CLOSED = 0.5;
+    //private static final double WR_OPEN = 1;
 
-    private static double targetPosition = 0;
+    //private static double targetPosition = 0;
 
     private static boolean end = false;
     @Override
@@ -37,11 +37,12 @@ public class Drive extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("fr");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("br");
 
-        CRServo intakeServo1 = hardwareMap.crservo.get("Lin");
-        CRServo intakeServo2 = hardwareMap.crservo.get("Rin");
+        CRServo wrist = hardwareMap.crservo.get("wrist");
+        CRServo fingers = hardwareMap.crservo.get("fingers");
         CRServo hook1 = hardwareMap.crservo.get("hook1");
         CRServo hook2 = hardwareMap.crservo.get("hook2");
-        Servo wrist = hardwareMap.servo.get("Wrist");
+        Servo elbow = hardwareMap.servo.get("elbow");
+        Servo shoulder = hardwareMap.servo.get("shoulder");
 
         //motors directions
         arm.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -49,7 +50,7 @@ public class Drive extends LinearOpMode {
         slide1.setDirection(DcMotor.Direction.FORWARD);
         frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        intakeServo2.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         //motors modes
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -69,7 +70,7 @@ public class Drive extends LinearOpMode {
 //        // Set Initial Servo Positions
 //        intakeServo1.setPosition(INTAKE_CLOSED_POSITION);
 //        intakeServo2.setPosition(INTAKE_OPEN_POSITION);
-        wrist.setPosition(WR_DF);
+        //wrist.setPosition(WR_DF);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -142,47 +143,58 @@ public class Drive extends LinearOpMode {
             }
             //arm
 
-        if(Math.abs(gamepad1.left_stick_y) >= 0.1){
+            if(Math.abs(gamepad1.left_stick_y) >= 0.1){
                 arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 arm.setPower(gamepad1.left_stick_y);
             }
-        else {
+            else {
 
-            int currentPos = arm.getCurrentPosition();
+                int currentPos = arm.getCurrentPosition();
 
-            if (arm.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
-                arm.setTargetPosition(currentPos);
-                arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                if (arm.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
+                    arm.setTargetPosition(currentPos);
+                    arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                }
+
+                arm.setPower(0.8);
             }
 
-            arm.setPower(0.8);
-        }
 
-
-//wrist 
+            //shoulder
 
             if (gamepad1.dpad_up) {
-                wrist.setPosition(WR_OPEN);
+                shoulder.setPosition(0);
             }
             if (gamepad1.dpad_right) {
-                wrist.setPosition(WR_DF);
+                shoulder.setPosition(0.5);
             }
             if (gamepad1.dpad_down) {
-                wrist.setPosition(WR_CLOSED);
+                shoulder.setPosition(1);
             }
 
-            //servos
-            if (gamepad1.square) {
-                intakeServo1.setPower(1);
-                intakeServo2.setPower(1);
+            //elbow
+            if (gamepad2.dpad_up) {
+                elbow.setPosition(0);
             }
-            else if(gamepad1.circle){
-                intakeServo1.setPower(-1);
-                intakeServo2.setPower(-1);
+            if (gamepad2.dpad_right) {
+                elbow.setPosition(0.5);
+            }
+            if (gamepad2.dpad_down) {
+                elbow.setPosition(1);
+            }
+
+            //wrist
+            if (gamepad1.triangle) {
+                wrist.setPower(0.5);
+                wrist.setPower(0.5);
+            }
+            else if(gamepad1.x){
+                wrist.setPower(-0.5);
+                wrist.setPower(-0.5);
             }
             else {
-                intakeServo1.setPower(0);
-                intakeServo2.setPower(0);
+                wrist.setPower(0);
+                wrist.setPower(0);
             }
             //hook
             if (gamepad2.dpad_down) {
