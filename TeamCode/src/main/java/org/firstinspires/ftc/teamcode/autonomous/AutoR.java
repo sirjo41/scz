441c6d6b51280c448b165bdd8ee5fcb5bea83476
siraj <srjbadri2008@gmail.com>
2025-02-20 16:42:44 +0200

    drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoR.java
@@ -118,50 +118,7 @@ public class AutoR extends LinearOpMode {
         telemetry.addData("Status", "Executing Autonomous Routine");
         telemetry.update();
 
-        gotostage(slide1,slide2,STAGE_OUTTAKE2);
-        Actions.runBlocking( OutTake1.build() );
-        gotostage2(slide1,slide2,STAGE_OUTTAKE);
-        fingers.setPosition(FINGERS_OPEN);
 
-        Actions.runBlocking( InTake2.build());
-        gotostage2(slide1,slide2,STAGE_INTAKE);
-        sleep(1000);
-        fingers.setPosition(FINGERS_CLOSE);
-
-        gotostage(slide1,slide2,STAGE_OUTTAKE2);
-        Actions.runBlocking(OutTake2.build());
-        gotostage2(slide1,slide2,STAGE_OUTTAKE);
-        fingers.setPosition(FINGERS_OPEN);
-
-//        Actions.runBlocking(InTake1.build());
-//        gotostage(slide1,slide2,STAGE_INTAKE);
-//        fingers.setPosition(FINGERS_CLOSE);
-//
-//        gotostage(slide1,slide2,STAGE_OUTTAKE2);
-//        Actions.runBlocking(OutTake3.build());
-//        gotostage(slide1,slide2,STAGE_OUTTAKE);
-//        fingers.setPosition(FINGERS_OPEN);
-//
-//        Actions.runBlocking(InTake1.build());
-//        gotostage(slide1,slide2,STAGE_INTAKE);
-//        fingers.setPosition(FINGERS_CLOSE);
-//
-//        gotostage(slide1,slide2,STAGE_OUTTAKE2);
-//        Actions.runBlocking(OutTake3.build());
-//        gotostage(slide1,slide2,STAGE_OUTTAKE);
-//        fingers.setPosition(FINGERS_OPEN);
-//
-//        Actions.runBlocking(InTake1.build());
-//        gotostage(slide1,slide2,STAGE_INTAKE);
-//        fingers.setPosition(FINGERS_CLOSE);
-//
-//        gotostage(slide1,slide2,STAGE_OUTTAKE2);
-//        Actions.runBlocking(OutTake3.build());
-//        gotostage(slide1,slide2,STAGE_OUTTAKE);
-//        fingers.setPosition(FINGERS_OPEN);
-//
-//        Actions.runBlocking(InTake1.build());
-//        gotostage(slide1,slide2,STAGE_INTAKE);
 
             if (isStopRequested()) return;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/pos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/pos.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
