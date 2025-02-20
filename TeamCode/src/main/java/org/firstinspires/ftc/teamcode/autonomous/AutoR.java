31f8d89f94c13a0fa8d2ff8e7cdfff7a93ae8926
siraj <srjbadri2008@gmail.com>
2025-02-20 17:00:21 +0200

    drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoR.java
@@ -110,8 +110,6 @@ public class AutoR extends LinearOpMode {
         telemetry.update();
         waitForStart();
         if (isStopRequested()) return;
-        arm.setPower(1);
-        sleep(900);
         fingers.setPosition(FINGERS_CLOSE);
         elbow.setPosition(ELBOW_INTAKE);
         shoulder.setPosition(SHOULDER_INTAKE);
